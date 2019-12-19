56b2120ffca519a90ea1eb0c8bd0a19873f6d979
wade_chen <wade_chen@ehsy.com>
2019-12-19 19:08:58 +0800

    commit

--- src/main/java/com/gen/mmall/service/IUserService.java
+++ src/main/java/com/gen/mmall/service/IUserService.java
@@ -1,5 +1,14 @@
 package com.gen.mmall.service;
 
+import com.gen.mmall.common.ServerResponse;
+import com.gen.mmall.pojo.User;
+
 public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str,String type);
 
 }
